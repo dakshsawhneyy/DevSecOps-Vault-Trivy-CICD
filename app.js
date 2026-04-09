6f3d7478e6484277957ca93a353cc62cdc9d96a6
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-04-09 15:08:46 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+// Simple route '/'
+app.get('/', (req,res) => {
+  res.send('Hello from Daksh!)
+})
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
+})
